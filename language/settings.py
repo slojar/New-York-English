784fc79243663588f0d4651027c88c3ff0f97c47
slojararshavin <sunday@tm30.net>
2024-05-04 00:04:11 +0100

    update

--- language/settings.py
+++ language/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
