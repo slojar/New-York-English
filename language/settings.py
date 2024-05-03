19d4021fc53bf2353cbd70ee72cf8802bf25456b
slojararshavin <sunday@tm30.net>
2024-05-03 23:23:18 +0100

    update

--- language/settings.py
+++ language/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env('SECRET_KEY', None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
